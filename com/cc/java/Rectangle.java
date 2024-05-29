1d13f2642cbde83dcacb84b6e724bb9046b9794a
Peter <schmidt.multimedia@gmail.com>
2024-05-29 10:30:04 +0200

    02 / Implementieren von area()

--- com/cc/java/Rectangle.java
+++ com/cc/java/Rectangle.java
@@ -1,6 +1,6 @@
 package com.cc.java;
 
-public class Rectangle {
+public class Rectangle extends Shape{
 
     private double height;
     private double width;
@@ -8,6 +8,11 @@ public class Rectangle {
     public Rectangle(double height, double width) {
         this.height = height;
         this.width = width;
+    }
+
+    @Override
+    public double area() {
+        return width * height;
     }
 
 
