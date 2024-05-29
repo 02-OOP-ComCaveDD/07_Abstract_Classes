ea7fa0e7d5d6f35f2331019cd27d3d2669b2517c
Peter <schmidt.multimedia@gmail.com>
2024-05-29 09:31:28 +0200

    01 / Shape + konkrete Klassen

---
+++ com/cc/java/Rectangle.java
@@ -0,0 +1,14 @@
+package com.cc.java;
+
+public class Rectangle {
+
+    private double height;
+    private double width;
+
+    public Rectangle(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+
+}
